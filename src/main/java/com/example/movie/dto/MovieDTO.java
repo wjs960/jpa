8ee5096642b0ce0e34a03c523a5c0d03fd8f34b2
wjs960 <wjs960@gmail.com>
2023-10-06 19:36:20 +0900

    frist

---
+++ src/main/java/com/example/movie/dto/MovieDTO.java
@@ -0,0 +1,18 @@
+package com.example.movie.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieDTO {
+
+    private String title; // 제목
+    private String produce; // 감독
+    private String producer; // 제작사
+    private String cast; // 출연진
+    private String content; // 줄거리
+    private LocalDateTime released; // 개봉일
+    private LocalDateTime enrollDate; // 현재날짜
+    private LocalDateTime dDay; // d-day
+    private String country; // 개봉국가
+
+
+}
